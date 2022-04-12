84512765ba868b5dc81569d2310f5a26143dd02e
DarianDickerson <DarianMD32@gmail.com>
2022-04-11 21:02:33 -0600

    Completed OnesAndZeros

---
+++ 7kyu/OnesAndZeros.js
@@ -0,0 +1,25 @@
+/*
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 1, 0] ==> 6
+Testing: [1, 1, 1, 1] ==> 15
+Testing: [1, 0, 1, 1] ==> 11
+However, the arrays can have varying lengths, not just limited to 4.
+*/
+
+const binaryArrayToNumber = arr => {
+    let count = arr.length
+    return arr.reduce(function(acc,b){
+      count--
+      return (b) ? acc += Math.pow(2,count) : acc += 0
+    },0) 
+}
