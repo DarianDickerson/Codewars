8962cc6e71217fcb5fc2fe35571e5fdde9d3a6be
DarianDickerson <DarianMD32@gmail.com>
2022-04-11 19:31:20 -0600

    Completed ComplementaryDNA

---
+++ 7kyu/ComplementaryDNA.js
@@ -0,0 +1,39 @@
+/*
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"
+dnaStrand []        `shouldBe` []
+dnaStrand [A,T,G,C] `shouldBe` [T,A,C,G]
+dnaStrand [G,T,A,T] `shouldBe` [C,A,T,A]
+dnaStrand [A,A,A,A] `shouldBe` [T,T,T,T]
+*/
+
+function DNAStrand(dna){
+    let mirror = []
+    for(let i=0; i<dna.length; i++){
+      switch(dna[i]){
+          case "A":
+            mirror += "T"
+            break
+          case "T":
+            mirror += "A"
+            break
+          case "C":
+            mirror += "G"
+            break
+          case "G":
+            mirror += "C"
+            break
+      }
+    }
+    return mirror
+}
