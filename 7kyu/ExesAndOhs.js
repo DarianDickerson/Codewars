2a3878b22083b992e45b183a85a3c457f780437b
DarianDickerson <DarianMD32@gmail.com>
2022-04-11 18:27:28 -0600

    Completed ExesAndOhs Kata

---
+++ 7kyu/ExesAndOhs.js
@@ -0,0 +1,27 @@
+/*
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+*/
+
+function XO(str) {
+    let x = 0
+    let o = 0
+    
+    for(let i=0; i<str.length; i++){
+      if(str[i] === "X" || str[i] === "x"){
+        x++
+      }else if(str[i] === "O" || str[i] === "o"){
+        o++
+      }
+    }
+  
+    return x === o ? true : false
+  
+}
