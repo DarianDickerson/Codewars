efdbc26c7505c632bb2334376c5138122fd80265
DarianDickerson <DarianMD32@gmail.com>
2022-05-18 11:18:05 -0600

    Completed AreaOfAnAnnulus 5/18/22

---
+++ 7kyu/AreaOfAnAnnulus.js
@@ -0,0 +1,12 @@
+/*
+Area of an annulus
+When given the length of the arrow as a, where a is an integer and ≥ 1, calculate the area of the annulus (the grey ring).
+
+annulus
+
+Round the answer to two decimal places. In case you need pi, you can use the standard Math.PI/M_PI.
+*/
+
+function annulusArea(a) {
+    return +(Math.PI * (a/2)**2).toFixed(2)
+}
