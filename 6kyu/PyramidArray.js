0ec44c8df3b998cfbb04d1857614f2f105c0d791
DarianDickerson <DarianMD32@gmail.com>
2022-06-01 17:25:14 -0600

    Completed PyramidArray 6/1/22

---
+++ 6kyu/PyramidArray.js
@@ -0,0 +1,19 @@
+/*
+Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+
+pyramid(0) => [ ]
+pyramid(1) => [ [1] ]
+pyramid(2) => [ [1], [1, 1] ]
+pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+Note: the subarrays should be filled with 1s
+*/
+
+function pyramid(n) {
+    let pyramid = []
+    
+    for(let i=0; i<n; i++){
+      pyramid.push(Array(i+1).fill(1))
+    }
+    
+    return pyramid
+}
