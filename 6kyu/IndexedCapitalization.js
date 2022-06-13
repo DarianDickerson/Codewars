d018756a87eb733276b2c309f41fe43b101d5d9d
DarianDickerson <DarianMD32@gmail.com>
2022-06-13 11:56:08 -0600

    Completed IndexedCapitalization 6/13/22

---
+++ 6kyu/IndexedCapitalization.js
@@ -0,0 +1,17 @@
+/*
+Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+For example:
+
+capitalize("abcdef",[1,2,5]) = "aBCdeF"
+capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+The input will be a lowercase string with no spaces and an array of digits.
+
+Good luck!
+*/
+
+function capitalize(s,arr){
+    s = s.split("")
+    arr.forEach( i => i>=0 && i<s.length ? s[i] = s[i].toUpperCase() : console.log("Nada"))
+    return s.join("")
+}
