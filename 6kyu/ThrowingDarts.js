74472daaddfe266eb1d94cbcc9d47ecb93ac5edd
DarianDickerson <DarianMD32@gmail.com>
2022-06-21 20:56:35 -0600

    Completed ThrowingDarts 6/21/22

---
+++ 6kyu/ThrowingDarts.js
@@ -0,0 +1,45 @@
+/*
+You've just recently been hired to calculate scores for a Dart Board game!
+
+Scoring specifications:
+
+0 points - radius above 10
+5 points - radius between 5 and 10 inclusive
+10 points - radius less than 5
+If all radii are less than 5, award 100 BONUS POINTS!
+
+Write a function that accepts an array of radii (can be integers and/or floats), and returns a total score using the above specification.
+
+An empty array should return 0.
+
+Examples:
+scoreThrows( [1, 5, 11] )    =>  15
+scoreThrows( [15, 20, 30] )  =>  0
+scoreThrows( [1, 2, 3, 4] )  =>  140
+*/
+
+function scoreThrows(radii){
+    let score = 0
+    
+    if(radii.length === 0){
+      return score
+    }
+    
+    score += radii.reduce((a,c)=>{
+      if(c < 5){
+        return a + 10
+      }
+      else if(c <= 10){
+        return a + 5
+      }
+      else{
+        return a + 0
+      }
+    },0)
+    
+    if(radii.length * 5 > radii.reduce((a,c)=>a+c)){
+      score += 100
+    }
+    
+    return score
+}
