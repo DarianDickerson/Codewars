6ff7b7d68e84851e526fee8a86a59b6f03a36453
DarianDickerson <DarianMD32@gmail.com>
2022-06-26 16:56:07 -0600

    Completed KataExampleTwist 6/26/22

---
+++ 8kyu/KataExampleTwist.js
@@ -0,0 +1,8 @@
+/*
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites/Websites 1,000 times.
+*/
+
+// add the value "codewars" to the websites array 1,000 times
+var websites = new Array(1000).fill("codewars")
