93a25507f6a2d2e7f1f74d09ae7634ddce359ba0
DarianDickerson <DarianMD32@gmail.com>
2022-07-04 19:46:52 -0600

    Completed FillingAnArray1 7/4/22

---
+++ 8kyu/FillingAnArray1.js
@@ -0,0 +1,18 @@
+/*
+We want an array, but not just any old array, an array with contents!
+
+Write a function that produces an array with the numbers 0 to N-1 in it.
+
+For example, the following code will result in an array containing the numbers 0 to 4:
+
+arr(5) // => [0,1,2,3,4]
+Note: The parameter is optional. So you have to give it a default value.
+*/
+
+const arr = N => {
+    let res = []
+    for(let i=0; i<N; i++){
+      res.push(i)
+    }
+    return res
+}
