5d57bacd6639ee1db34b1b081c76f5014830cb98
DarianDickerson <DarianMD32@gmail.com>
2022-07-07 20:42:55 -0600

    Completed DrawStairs 7/7/22

---
+++ 8kyu/DrawStairs.js
@@ -0,0 +1,31 @@
+/*
+Given a number n, draw stairs using the letter "I", n tall and n wide, with the tallest in the top left.
+
+For example n = 3 result in:
+
+"I\n I\n  I"
+or printed:
+
+I
+ I
+  I
+Another example, a 7-step stairs should be drawn like this:
+
+I
+ I
+  I
+   I
+    I
+     I
+      I
+*/
+
+function drawStairs(n) {
+    let stairs = ""
+    let step = "I\n"
+    for(let i=0; i<n; i++){
+      stairs += step
+      step = " " + step
+    }
+    return stairs.substring(0,stairs.length-1)
+}
