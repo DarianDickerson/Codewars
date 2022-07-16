5aca9aa763af7749a1f3aed3d19ac1d29f803489
DarianDickerson <DarianMD32@gmail.com>
2022-07-16 13:52:00 -0600

    Completed EnumerableMagic4TrueOrNone 7/16/22

---
+++ 8kyu/EnumerableMagic4TrueOrNone.js
@@ -0,0 +1,14 @@
+/*
+Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns true if the block (/function) returns true for none of the items in the array. An empty list should return true.
+*/
+
+function none(arr, fun){
+  
+    for(let i in arr){
+      if(fun(arr[i])){
+        return false
+      }
+    }
+    
+    return true
+}
