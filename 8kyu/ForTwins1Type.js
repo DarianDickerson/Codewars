ab66bc0ae71154523496346e63a003f207bc0ef4
DarianDickerson <DarianMD32@gmail.com>
2022-07-26 11:16:56 -0600

    Completed ForTwins1Type 7/26/22

---
+++ 8kyu/ForTwins1Type.js
@@ -0,0 +1,12 @@
+/*
+Task:
+Write function typeValidation that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+
+Examples:
+typeValidation(42, "number");   // => true
+typeValidation("42", "number"); // => false
+*/
+
+function typeValidation(variable, type) {
+    return type == typeof variable
+}
