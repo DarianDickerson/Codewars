64894e9a6c776bcad9fe8a9dac159737cf7e1af2
DarianDickerson <DarianMD32@gmail.com>
2022-08-06 19:53:09 -0600

    Completed BrokenCounter 8/6/22

---
+++ 8kyu/BrokenCounter.js
@@ -0,0 +1,17 @@
+//Our counter prototype is broken. Can you spot, what's wrong here?
+
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase = function() {
+  this.value++;
+};
+
+Counter.prototype.getValue = function() {
+  return this.value;
+};
+
+Counter.prototype.reset = function() {
+  this.value = 0;
+};
