1ec9dcb93c9916f43b6298491c7d063e3c6fe744
DarianDickerson <DarianMD32@gmail.com>
2022-08-15 17:59:16 -0600

    Completed FinishGuessNumberGame 8/15/22

---
+++ 8kyu/FinishGuessNumberGame.js
@@ -0,0 +1,26 @@
+/*
+Imagine you are creating a game where the user has to guess the correct number. But there is a limit of how many guesses the user can do.
+
+If the user tries to guess more than the limit, the function should throw an error.
+If the user guess is right it should return true.
+If the user guess is wrong it should return false and lose a life.
+Can you finish the game so all the rules are met?
+*/
+
+class Guesser {
+    constructor(number, lives) {
+      this.number = number;
+      this.lives = lives;
+    }
+    
+    guess(n) {
+      if(this.lives <= 0) throw "You're out of lives"
+      else if(n === this.number){
+        return true
+      }
+      else{
+        this.lives--
+        return false
+      }
+    }
+}
