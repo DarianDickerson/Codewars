655c51823382583af8e101ea2ab71088a44cf5bc
DarianDickerson <DarianMD32@gmail.com>
2022-08-29 19:57:31 -0600

    Completed FuelCalculator 8/29/22

---
+++ 8kyu/FuelCalculator.js
@@ -0,0 +1,19 @@
+/*
+In this kata you will have to write a function that takes litres and pricePerLitre (in dollar) as arguments.
+
+Purchases of 2 or more litres get a discount of 5 cents per litre, purchases of 4 or more litres get a discount of 10 cents per litre, and so on every two litres, up to a maximum discount of 25 cents per litre. But total discount per litre cannot be more than 25 cents. Return the total cost rounded to 2 decimal places. Also you can guess that there will not be negative or non-numeric inputs.
+
+Good Luck!
+
+Note
+1 Dollar = 100 Cents
+*/
+
+function fuelPrice(l, ppl) {
+    if(l >= 10) return Math.round((l * (ppl - .25))*100)/100
+    else if(l >= 8) return Math.round((l * (ppl - .20))*100)/100
+    else if(l >= 6) return Math.round((l * (ppl - .15))*100)/100
+    else if(l >= 4) return Math.round((l * (ppl - .10))*100)/100
+    else if(l >= 2) return Math.round((l * (ppl - .05))*100)/100
+    else return Math.round(l * ppl *100)/100
+}
