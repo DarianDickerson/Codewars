57a78e78ee94526fb8304386c79259d5438ea123
DarianDickerson <DarianMD32@gmail.com>
2022-10-04 15:25:05 -0600

    Completed RegexCountLowercase 10/4/22

---
+++ 8kyu/RegexCountLowercase.js
@@ -0,0 +1,21 @@
+/*
+Your task is simply to count the total number of lowercase letters in a string.
+
+Examples
+lowercaseCount("abc"); ===> 3
+
+lowercaseCount("abcABC123"); ===> 3
+
+lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 3
+
+lowercaseCount(""); ===> 0;
+
+lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 0
+
+lowercaseCount("abcdefghijklmnopqrstuvwxyz"); ===> 26
+*/
+
+function lowercaseCount(str){
+    let abc = "abcdefghijklmnopqrstuvwxyz"
+    return str.split("").filter(ch => abc.includes(ch)).length
+}
