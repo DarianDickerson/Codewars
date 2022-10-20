ae8100fba24f88e33fbca003a1ec8523e5f32011
DarianDickerson <DarianMD32@gmail.com>
2022-10-20 15:37:50 -0600

    Completed ColorGhost 10/20/22

---
+++ 8kyu/ColorGhost.js
@@ -0,0 +1,17 @@
+/*
+Color Ghost
+Create a class Ghost
+
+Ghost objects are instantiated without any arguments.
+
+Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+ghost = new Ghost();
+ghost.color //=> "white" or "yellow" or "purple" or "red"
+*/
+
+let color = ["white","yellow","purple","red"]
+
+var Ghost = function() {
+  this.color = color[Math.floor(Math.random()*4)]
+};
