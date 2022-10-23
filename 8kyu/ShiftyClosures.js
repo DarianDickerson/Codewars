01376e2319ed79429401ba451c5277e44da743b9
DarianDickerson <DarianMD32@gmail.com>
2022-10-23 15:50:23 -0600

    Completed ShiftyClosures 10/23/22

---
+++ 8kyu/ShiftyClosures.js
@@ -0,0 +1,14 @@
+/*
+Functional closures can get overly attached. Set them straight!
+
+Why doesn't greet_abe() actually greet Abe?
+*/
+
+var name = 'Abe';
+var greet_abe = function() {
+  return "Hello, " + "Abe" + '!'
+}
+name = 'Ben';
+var greet_ben = function() {
+  return "Hello, " + name + '!';
+};
