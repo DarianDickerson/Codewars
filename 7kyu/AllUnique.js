d9461d451da989a1736be6e67e8ec684f9b8c306
DarianDickerson <DarianMD32@gmail.com>
2022-12-06 13:56:03 -0700

    Completed AllUnique 12/6/22

---
+++ 7kyu/AllUnique.js
@@ -0,0 +1,14 @@
+/*
+Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+
+The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+*/
+
+function hasUniqueChars(str){
+    let unique = new Set()
+    return !str.split("").some(ch =>{
+      if(unique.has(ch)) return true
+      unique.add(ch)
+      return false
+    })
+}
