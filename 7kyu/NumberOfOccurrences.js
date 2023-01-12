4292cf463d8c58e3fde6f8ec1aaa62af36ae78b1
DarianDickerson <DarianMD32@gmail.com>
2023-01-12 13:29:27 -0700

    Completed NumberOfOccurrences 1/12/23

---
+++ 7kyu/NumberOfOccurrences.js
@@ -0,0 +1,16 @@
+/*
+Write a function that returns the number of occurrences of an element in an array.
+
+Examples
+var arr = [0, 1, 2, 2, 3];
+arr.numberOfOccurrences(0) === 1;
+arr.numberOfOccurrences(4) === 0;
+arr.numberOfOccurrences(2) === 2;
+arr.numberOfOccurrences("a") === 0;
+*/
+
+Array.prototype.numberOfOccurrences = function(o) {
+    let count = 0 
+    this.forEach(item =>  o === item ? count++ : count += 0)
+    return count
+}
