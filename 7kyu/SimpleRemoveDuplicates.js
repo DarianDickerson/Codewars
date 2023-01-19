dbfc14b4edaf70085aaea49ed1f51f56bcb01ec4
DarianDickerson <DarianMD32@gmail.com>
2023-01-19 14:34:34 -0700

    Completed SimpleRemoveDuplicates 1/19/23

---
+++ 7kyu/SimpleRemoveDuplicates.js
@@ -0,0 +1,27 @@
+/*
+Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+
+Example:
+For input: [3, 4, 4, 3, 6, 3]
+
+remove the 3 at index 0
+remove the 4 at index 1
+remove the 3 at index 3
+Expected output: [4, 6, 3]
+
+More examples can be found in the test cases.
+
+Good luck!
+
+
+*/
+
+function solve(arr) {
+    let res = []
+    
+    arr.reverse().forEach(num =>{
+      if(!res.includes(num)) res.push(num)
+    })
+    
+    return res.reverse();
+}
