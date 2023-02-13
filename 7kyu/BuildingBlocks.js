9d1736834e14c30c12f939d8b34a8efb41014128
DarianDickerson <DarianMD32@gmail.com>
2023-02-12 23:22:13 -0700

    Completed BuildingBlocks 2/12/23

---
+++ 7kyu/BuildingBlocks.js
@@ -0,0 +1,62 @@
+/*
+Write a class Block that creates a block (Duh..)
+
+Requirements
+The constructor should take an array as an argument, this will contain 3 integers of the form [width, length, height] from which the Block should be created.
+
+Define these methods:
+
+`getWidth()` return the width of the `Block`
+
+`getLength()` return the length of the `Block`
+
+`getHeight()` return the height of the `Block`
+
+`getVolume()` return the volume of the `Block`
+
+`getSurfaceArea()` return the surface area of the `Block`
+Examples
+    let b = new Block([2,4,6]) -> creates a `Block` object with a width of `2` a length of `4` and a height of `6`
+    b.getWidth() // -> 2
+    
+    b.getLength() // -> 4
+    
+    b.getHeight() // -> 6
+    
+    b.getVolume() // -> 48
+    
+    b.getSurfaceArea() // -> 88
+Note: no error checking is needed
+
+Any feedback would be much appreciated
+*/
+
+
+class Block{
+    constructor(data){
+      this.data = data
+    }
+    
+    getWidth(){
+      return this.data[0]
+    }
+    
+    getLength(){
+      return this.data[1]
+    }
+    
+    getHeight(){
+      return this.data[2]
+    }
+    
+    getVolume(){
+      return this.data.reduce((a,c)=>a * c,1)
+    }
+    
+    getSurfaceArea(){
+      let area = this.data[0] * this.data[1] * 2
+      area += this.data[0] * this.data[2] * 2
+      area += this.data[1] * this.data[2] * 2
+      return area
+    }
+}
