da774e9030b38d1ed370ea044ef68d07f9b5ff2b
DarianDickerson <DarianMD32@gmail.com>
2023-02-20 00:57:16 -0700

    Completed AreaOfCircle 2/20/23

---
+++ 7kyu/AreaOfCircle.js
@@ -0,0 +1,16 @@
+/*
+Complete the function circleArea so that it will return the area of a circle with the given radius. Round the returned number to two decimal places (except for Haskell). If the radius is not positive or not a number, return false.
+
+Example:
+
+circleArea(-1485.86);    //returns false
+circleArea(0);           //returns false
+circleArea(43.2673);     //returns 5881.25
+circleArea(68);          //returns 14526.72
+circleArea("number");    //returns false
+*/
+
+var circleArea = function(radius) {
+    if(radius <= 0 || typeof radius != "number") return false
+    return Math.round((Math.PI * radius**2) * 100)/100
+};
