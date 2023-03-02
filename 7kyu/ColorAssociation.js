faa0227dbbebfc9b5aa0eb128af4a4ad13a63ee1
DarianDickerson <DarianMD32@gmail.com>
2023-03-02 16:32:17 -0700

    Completed ColorAssociation 3/2/23

---
+++ 7kyu/ColorAssociation.js
@@ -0,0 +1,20 @@
+/*
+Colour plays an important role in our lifes. Most of us like this colour better then another. User experience specialists believe that certain colours have certain psychological meanings for us.
+
+You are given a 2D array, composed of a colour and its 'common' association in each array element. The function you will write needs to return the colour as 'key' and association as its 'value'.
+
+For example:
+
+var array = [["white", "goodness"], ...] //returns [{white: 'goodness'}, ...]
+*/
+
+function colourAssociation(array){
+    let res = []
+    array.forEach(c => {
+      let push = new Object()
+      push[c[0]] = c[1]
+      res.push(push)
+    })
+    
+    return res
+}
