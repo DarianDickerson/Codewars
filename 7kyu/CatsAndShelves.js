3e05ee7067a394640dcd7b84e3dc9c833f1151d2
DarianDickerson <DarianMD32@gmail.com>
2023-03-30 22:42:26 -0600

    Completed CatsAndShelves 3/30/23

---
+++ 7kyu/CatsAndShelves.js
@@ -0,0 +1,47 @@
+/*
+Description
+An infinite number of shelves are arranged one above the other in a staggered fashion.
+The cat can jump either one or three shelves at a time: from shelf i to shelf i+1 or i+3 (the cat cannot climb on the shelf directly above its head), according to the illustration:
+
+                 ┌────────┐
+                 │-6------│
+                 └────────┘
+┌────────┐       
+│------5-│        
+└────────┘  ┌─────► OK!
+            │    ┌────────┐
+            │    │-4------│
+            │    └────────┘
+┌────────┐  │
+│------3-│  │     
+BANG!────┘  ├─────► OK! 
+  ▲  |\_/|  │    ┌────────┐
+  │ ("^-^)  │    │-2------│
+  │ )   (   │    └────────┘
+┌─┴─┴───┴┬──┘
+│------1-│
+└────────┘
+Input
+Start and finish shelf numbers (always positive integers, finish no smaller than start)
+
+Task
+Find the minimum number of jumps to go from start to finish
+
+Example
+Start 1, finish 5, then answer is 2 (1 => 4 => 5 or 1 => 2 => 5)
+*/
+
+function solution(start, finish) {
+    let pos = start
+    let count = 0
+    while(pos < finish){
+      if(finish-2 > pos){
+        pos += 3
+      }
+      else{
+        pos++
+      }
+      count++
+    }
+    return count
+}
