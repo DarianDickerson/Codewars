f64a2d7ee68c7e42cdb185e6fb279c91e47826f5
DarianDickerson <DarianMD32@gmail.com>
2023-04-29 16:32:17 -0600

    Completed WhoIsTheKiller 4/29/23

---
+++ 7kyu/WhoIsTheKiller.js
@@ -0,0 +1,33 @@
+/*
+Who is the killer?
+Some people have been killed!
+You have managed to narrow the suspects down to just a few. Luckily, you know every person who those suspects have seen on the day of the murders.
+
+Task.
+Given a dictionary with all the names of the suspects and everyone that they have seen on that day which may look like this:
+
+{'James': ['Jacob', 'Bill', 'Lucas'],
+ 'Johnny': ['David', 'Kyle', 'Lucas'],
+ 'Peter': ['Lucy', 'Kyle']}
+and also a list of the names of the dead people:
+
+['Lucas', 'Bill']
+return the name of the one killer, in our case 'James' because he is the only person that saw both 'Lucas' and 'Bill'
+*/
+
+function killer(suspectInfo, dead) {
+  
+    for(let sus in suspectInfo){
+      let innocent = false
+      for(let i=0; i<dead.length; i++){
+        if(!suspectInfo[sus].includes(dead[i])){
+          innocent = true
+          break
+        }
+      }
+      if(innocent) continue
+      return sus
+    }
+    
+    return ""
+}
