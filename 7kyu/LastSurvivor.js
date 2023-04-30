60c8969ab7770fe3b279a15aa7f610c05ea22bbf
DarianDickerson <DarianMD32@gmail.com>
2023-04-30 16:15:56 -0600

    Completed LastSurvivor 4/30/23

---
+++ 7kyu/LastSurvivor.js
@@ -0,0 +1,25 @@
+/*
+You are given a string of letters and an array of numbers.
+The numbers indicate positions of letters that must be removed, in order, starting from the beginning of the array.
+After each removal the size of the string decreases (there is no empty space).
+Return the only letter left.
+
+Example:
+
+let str = "zbk", arr = [0, 1]
+    str = "bk", arr = [1]
+    str = "b", arr = []
+    return 'b'
+*/
+
+function lastSurvivor(letters, coords) {
+    let pos = 0
+    let arr = letters.split("")
+    
+    while(pos < coords.length && arr.length > 1){
+      arr.splice(coords[pos], 1)
+      pos++
+    }
+    
+    return arr[0]
+}
