e2ae8c3d4860172837332e284f4c1ccd55da60af
DarianDickerson <DarianMD32@gmail.com>
2023-05-04 21:30:16 -0600

    Completed NameArrCapping 5/4/23

---
+++ 7kyu/NameArrCapping.js
@@ -0,0 +1,12 @@
+/*
+Create a method that accepts an array of names, and returns an array of each name with its first letter capitalized.
+
+example
+
+capMe(['jo', 'nelson', 'jurie'])     // returns ['Jo', 'Nelson', 'Jurie']
+capMe(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kelsey']
+*/
+
+function capMe(names) {
+    return names.map(n=> n[0].toUpperCase() + n.substr(1).toLowerCase())
+}
