138f6cbedcbf7834d7f6454b16b932af0f04ef92
DarianDickerson <DarianMD32@gmail.com>
2023-05-06 19:35:23 -0600

    Completed KeepTheOrder 5/6/23

---
+++ 7kyu/KeepTheOrder.js
@@ -0,0 +1,21 @@
+/*
+Task:
+Your job here is to write a function (keepOrder in JS/CoffeeScript, keep_order in Ruby/Crystal/Python, keeporder in Julia), which takes a sorted array ary and a value val, and returns the lowest index where you could insert val to maintain the sorted-ness of the array. The input array will always be sorted in ascending order. It may contain duplicates.
+
+Do not modify the input.
+
+Some examples:
+keepOrder([1, 2, 3, 4, 7], 5) //=> 4
+                      ^(index 4)
+keepOrder([1, 2, 3, 4, 7], 0) //=> 0
+          ^(index 0)
+keepOrder([1, 1, 2, 2, 2], 2) //=> 2
+                ^(index 2)
+
+*/                
+
+function keepOrder(ary, val) {
+    let i = ary.findIndex(n=> val <= n)
+    
+    return i === -1 ? ary.length : i
+}
