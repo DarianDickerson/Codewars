2e391afd3efb2aaca04c32cc13ab049f9965b06f
DarianDickerson <DarianMD32@gmail.com>
2023-05-06 19:21:38 -0600

    Completed GreenGlassDoor 5/6/23

---
+++ 7kyu/GreenGlassDoor.js
@@ -0,0 +1,27 @@
+/*
+Step through my green glass door.
+
+You can take the moon, but not the sun.
+
+You can take your slippers, but not your sandals.
+
+You can go through yelling, but not shouting.
+
+You can't run through fast, but you can run with speed.
+
+You can take a sheet, but not your blanket.
+
+You can wear your glasses, but not your contacts.
+
+Have you figured it out? Good! Then write a program that can figure it out as well.
+*/
+
+function stepThroughWith(s) {
+    if(s.length < 2) return false
+    
+    for(let i=1; i<s.length; i++){
+      if(s[i] === s[i-1]) return true
+    }
+    
+    return false
+}
