1ab69165d7f5c07e506690b1eee4cb7beb5531b8
DarianDickerson <DarianMD32@gmail.com>
2023-05-09 21:48:11 -0600

    Completed TheSpanFunction 5/9/23

---
+++ 7kyu/TheSpanFunction.js
@@ -0,0 +1,28 @@
+/*
+The span function is a good one to know. It accepts a sequence and a predicate function and returns two sequences. The first sequence contains all the elements of the argument sequence up to the item that caused the first failure of the predicate. The second returned sequence contains the rest of the original sequence. The original sequence is not modified.
+
+For example,
+
+
+function isEven (x) {
+  return Math.abs(x) % 2 === 0;
+}
+
+var arr = [2,4,6,1,8,10];
+
+// This is true
+span(arr, isEven) === [[2,4,6],[1,8,10]]
+Your task is to...wait for it... write your own span function !!!
+*/
+
+function span(arr, predicate) {
+    let yes = [], no = [], pass = true 
+    for(let i=0; i<arr.length; i++){
+      if(!predicate(arr[i])) pass = false
+      
+      if(pass) yes.push(arr[i])
+      else no.push(arr[i])
+    }
+    
+    return [yes, no]
+}
